15fbe3e3a58963570590b3ec4cc0ece3e3f08454
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-08-13 13:10:25 +0200

    Update arduino_controller.py

--- mobile_base_controllers/arduino_controller.py
+++ mobile_base_controllers/arduino_controller.py
@@ -6,12 +6,15 @@ class ArduinoMotorController:
         self.serial = Serial(port, baudrate=115200)
 
     def mode_idle(self, axis):
+        axis = 1 - axis
         self.send_command(f'w axis{axis}.requested_state 1')
 
     def mode_close_loop_control(self, axis):
+        axis = 1 - axis
         self.send_command(f'w axis{axis}.requested_state 8')
 
     def move_input_vel(self, axis, vel):
+        axis = 1 - axis
         self.send_command(f'v {axis} {vel:.2f} 0')
 
     def send_command(self, command):
